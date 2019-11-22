db4c3246b44adad36e947341cb7c316bdd2e6a88
AlePur <35966625+AlePur@users.noreply.github.com>
2019-11-22 22:33:41 +0100

    Create duo.js

---
+++ duo.js
@@ -0,0 +1,62 @@
+const EventEmitter = require('events');
+const request = require('request');
+
+function Api(user, pass) {
+  this.app = new EventEmitter();
+  this.token = (() => {
+    return new Promise((resolve, reject) => {
+      request.post({
+        url: 'https://www.duolingo.com/login',
+        json: {
+          "login": user,
+          "password": pass
+        }
+      }, (err, res, body) => {
+        const { statusCode } = res;
+        let error;
+        if (statusCode !== 200) {
+          error = new Error('Request Failed.\n' +
+                            `Status Code: ${statusCode}`);
+        }
+        if (error) {
+          throw error;
+        }
+        resolve(res.headers['jwt']);
+      });
+    });
+  })();
+  this.ready = (() => {
+    this.token.then((x) => {
+      this.token = x;
+      this.app.emit('ready');
+    });
+  })();
+  this.getUserObject = (name) => {
+    return new Promise((resolve, reject) => {
+      request.get({
+        url: 'https://www.duolingo.com/users/'+name,
+        headers: {
+          'Authorization': this.token
+        }
+      }, (err, res, body) => {
+        const { statusCode } = res;
+        let error;
+        if (statusCode !== 200) {
+          error = new Error('Request Failed.\n' +
+                            `Status Code: ${statusCode}`);
+        }
+        if (error) {
+          console.error(error.message);
+          // Consume response data to free up memory
+          res.resume();
+          return;
+        }
+        resolve(JSON.parse(body));
+      });
+    });
+  }
+}
+
+module.exports = {
+  Api: Api
+}
